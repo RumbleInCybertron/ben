50d5e09805da6b4f4ec4ae8cf8f45ed0c049ebb2
Derek <rumbleincybertron@gmail.com>
2024-11-26 19:08:36 +0900

    log errors -> auth

--- backend/user_auth_service/app/exceptions.py
+++ backend/user_auth_service/app/exceptions.py
@@ -1,7 +1,11 @@
+import logging
 from fastapi import HTTPException, status
+
+logger = logging.getLogger("user_auth_service")
 
 class UserAlreadyExistsException(HTTPException):
     def __init__(self):
+        logger.error("Attempt to create a user with an existing username")
         super().__init__(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="User already exists"
@@ -9,6 +13,7 @@ class UserAlreadyExistsException(HTTPException):
 
 class InvalidCredentialsException(HTTPException):
     def __init__(self):
+        logger.warning("Invalid login attempt")
         super().__init__(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid username or password"
@@ -16,6 +21,7 @@ class InvalidCredentialsException(HTTPException):
 
 class DatabaseConnectionException(HTTPException):
     def __init__(self):
+        logger.critical("Database connection failed")
         super().__init__(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Could not connect to the database"
